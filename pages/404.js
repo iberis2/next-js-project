dcb15fd6abca40b973df08bb42493e4567533e57
iberis2 <snow9402@gmail.com>
2023-08-01 16:45:29 +0900

    Feat: 404 Not Found page 생성
    - pages 폴더 하위에 404.js 파일 생성

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import ButtonLink from '@/components/ButtonLink'
+import Container from '@/components/Container'
+import Header from '@/components/Header'
+import styles from '@/styles/NotFound.module.css'
+
+export default function NotFound() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className={styles.notFound}>
+          <div className={styles.content}>
+            페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.
+            <br />
+            입력하신 주소가 정확한지 다시 한 번 확인해주시기 바랍니다.
+          </div>
+          <ButtonLink className={styles.button} href='/'>
+            홈으로 이동
+          </ButtonLink>
+        </div>
+      </Container>
+    </>
+  )
+}
