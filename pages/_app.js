2ad5952d51381a09674b513cc7f20790f9564da3
iberis2 <snow9402@gmail.com>
2023-08-03 01:26:22 +0900

    Feat: 구글 Noto_Sans_KR  폰트 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
+import { Noto_Sans_KR } from 'next/font/google'
 import Container from '@/components/Container'
 import Header from '@/components/Header'
 import { ThemeProvider } from '@/lib/ThemeContext'
 import '@/styles/global.css'
-import Head from 'next/head'
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: [],
+})
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,12 +17,14 @@ export default function App({ Component, pageProps }) {
         <title>NextJs Shopping Mall</title>
         <link href='/nextIcon.ico' rel='icon' />
       </Head>
-      <ThemeProvider>
-        <Header />
-        <Container>
-          <Component {...pageProps} />
-        </Container>
-      </ThemeProvider>
+      <main className={notoSansKR.className}>
+        <ThemeProvider>
+          <Header />
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </ThemeProvider>
+      </main>
     </>
   )
 }
