60bbef593aaf2f7b81c6118e5ff324104834fb8a
iberis2 <snow9402@gmail.com>
2023-08-02 23:55:13 +0900

    Refactor: next/Image 태크로 image 최적화

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -4,6 +4,7 @@ import { getData, getReviews } from '@/lib/apis'
 import SizeReviewList from '@/components/SizeReviewList'
 import StarRating from '@/components/StarRating'
 import styles from '@/styles/Product.module.css'
+import Image from 'next/image'
 
 export default function Product() {
   const [product, setProduct] = useState()
@@ -36,8 +37,8 @@ export default function Product() {
         <span className={styles.englishName}>{product.englishName}</span>
       </h1>
       <div className={styles.content}>
-        <div>
-          <img className={styles.image} src={product.imgUrl} alt={product.name} />
+        <div className={styles.imageContainer}>
+          <Image fill className={styles.image} src={product.imgUrl} alt={product.name} />
         </div>
         <div>
           <section className={styles.section}>
